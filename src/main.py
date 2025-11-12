705a8115f9d21b2b03fe1bf899a0b914bd29b1a9
Stephen-Rosario <sr68334@usc.edu>
2025-11-12 15:53:28 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import os
 from src.load import load_student_data
-# from src.worldbank import fetch_indicator 
+from src.worldbank import fetch_indicator
 
 def main():
+    # Load student data
     data_dir = os.path.join("data")
     math_path = os.path.join(data_dir, "student-mat.csv")
     por_path = os.path.join(data_dir, "student-por.csv")
@@ -11,8 +12,15 @@ def main():
     students = load_student_data(math_path, por_path)
     print(f"Loaded {students.shape[0]} rows and {students.shape[1]} columns")
 
+    # Fetch World Bank indicators
+    print("\n Fetching macro-level education data...")
+    gov_spend = fetch_indicator("SE.XPD.TOTL.GD.ZS")  # Govt expenditure
+    enroll_rate = fetch_indicator("SE.TER.ENRR")      # Tertiary enrollment
 
-    print(students.head())
+    print("Government Spending Data:")
+    print(gov_spend.head())
+    print("\nEnrollment Rate Data:")
+    print(enroll_rate.head())
 
 if __name__ == "__main__":
     main()
